d5b9e203e4a422c5f5b499a263ddf9d9555b2229
aavshr <aavash09@gmail.com>
2020-06-15 11:54:49 +0200

    Revert "fix regex for skip paths"
    This reverts commit 8708a43e4b5f91eba3a7ba34845bdc1aff8620c7.

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -65,7 +65,6 @@ func deploy(cmd *cobra.Command, args []string) error {
 	}
 
 	if c != nil {
-		fmt.Println("Deploying...")
 		_, err = client.Deploy(&api.DeployRequest{
 			ProgramID: progInfo.ID,
 			Changes:   c.Changes,
