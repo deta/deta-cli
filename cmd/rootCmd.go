7714903e5b5c2606f7946e4a3b7c11a32a55da77
aavshr <aavash09@gmail.com>
2020-10-19 18:04:22 +0200

    do not show usage on errors

--- cmd/rootCmd.go
+++ cmd/rootCmd.go
@@ -17,6 +17,8 @@ Complete documentation available at https://docs.deta.sh`,
 		Run: func(cmd *cobra.Command, args []string) {
 			cmd.Usage()
 		},
+		// no usage shown on errors
+		SilenceUsage: true,
 	}
 
 	// deta client
@@ -29,7 +31,6 @@ Complete documentation available at https://docs.deta.sh`,
 // Execute xx
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		os.Stderr.WriteString(err.Error())
 		os.Exit(1)
 	}
 }
