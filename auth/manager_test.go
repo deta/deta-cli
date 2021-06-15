4ab06f09d7ddd008260478f580db23894986162e
Mweya Ruider <mweyaruider@gmail.com>
2021-06-15 11:07:44 +0200

    auth: removed unneeded manager tests

--- auth/manager_test.go
+++ auth/manager_test.go
@@ -8,60 +8,6 @@ import (
 
 const jwt = "eyJraWQiOiJzTjVnTk43cWFGVmpPYVwvc3oyVTJvdnNIMTZyNThQb2RQVFpRZWlBQUhNZz0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJlMjY1YmNhMS03NGZiLTQ2MjQtOWVlMC0zOGE5ZmQ5YTQ4OTUiLCJldmVudF9pZCI6Ijk4MjUxMzQ3LTFmN2ItNGY0OC1hNTNhLWE3MjZhOWEzOTFiNiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2MjM0MDYwODQsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5ldS1jZW50cmFsLTEuYW1hem9uYXdzLmNvbVwvZXUtY2VudHJhbC0xX1ZhSGgwRW9YNSIsImV4cCI6MTYyMzQyNzY4NCwiaWF0IjoxNjIzNDA2MDg0LCJqdGkiOiIxOWUwMTZmOS05NGU0LTQ1ZTYtYmE4OS0xYjg4Y2ZmMThmN2QiLCJjbGllbnRfaWQiOiI0aW8zYWVxZjFoOTY3dWZhbGs1bjc0MmNmaiIsInVzZXJuYW1lIjoibXdleWEifQ.I5Nn5selXHZVPIJFJ0HSAoqUtQxz9s37e-6YLua2S0M9xcNhx0h-Yr6A3S8JNH4OXsCKYK3r-Y9TOjce4CGRQRTprDIKQnwS3RMOz1jTT6YVXy5n8CEvN2ySC8y-27oLLOjAHLGQjRaj_o8PkrQVSzSxKJbthTS0fNodPTsBt6FpXEZv5ULIJzFnWVxjPD3Rb0h6Ts-iYylvcurWiCKZYQLItuydoCG_99uuIlL1BzDHxBz-QRJYRF_gFJlQbUBfsXHv3VT_ET5vTkXeNILqdq_ON2PrcbXR1uDXm2wvF4TKZsD3tFRFEeH6FK6xM0dnMnogphEowZyU_BIEQlgTsw"
 
-func TestCreateToken(t *testing.T) {
-	_ = Token{
-		AccessToken:     jwt,
-		IDToken:         "idToken",
-		RefreshToken:    "refreshToken",
-		Expires:         0,
-		DetaAccessToken: "aaaaaaaaaaaaaaaaaaaaaaaaaa",
-	}
-	t.Logf("Token created!")
-}
-
-func TestCreateAndWriteToken(t *testing.T) {
-	token := Token{
-		AccessToken:     jwt,
-		IDToken:         "idToken",
-		RefreshToken:    "refreshToken",
-		Expires:         0,
-		DetaAccessToken: "aaaaaaaaaaaaaaaaaaaaaaaaaa",
-	}
-	token.AccessToken = "changed"
-	token.IDToken = "changed"
-	token.RefreshToken = "changed"
-	token.Expires = 999
-	token.DetaAccessToken = "changed"
-	t.Logf("Fields modified!")
-}
-
-func TestCreateAndReadToken(t *testing.T) {
-	token := Token{
-		AccessToken:     jwt,
-		IDToken:         "idToken",
-		RefreshToken:    "refreshToken",
-		Expires:         0,
-		DetaAccessToken: "aaaaaaaaaaaaaaaaaaaaaaaaaa",
-	}
-
-	if (token.AccessToken != jwt) {
-		t.Errorf("token.AccessToken == %s, want: %s", token.AccessToken, jwt)
-	}
-	if (token.IDToken != "idToken") {
-		t.Errorf("token.IDToken == %s, want: %s", token.IDToken, "idToken")
-	}
-	if (token.RefreshToken != "refreshToken") {
-		t.Errorf("token.RefreshToken == %s, want: %s", token.RefreshToken, "refreshToken")
-	}
-	if (token.Expires != 0) {
-		t.Errorf("token.Expires == %d, want: %d", token.Expires, 0)
-	}
-	if (token.DetaAccessToken != "aaaaaaaaaaaaaaaaaaaaaaaaaa") {
-		t.Errorf("token.DetaAccessToken == %s, want: %s", token.DetaAccessToken, "aaaaaaaaaaaaaaaaaaaaaaaaaa")
-	}
-	t.Logf("Info in token is as expected")
-}
-
 func TestCreateManager(t *testing.T) {
 	_ = NewManager()
 	t.Logf("Manager created!")
