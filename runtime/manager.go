029688e7f30a1e2454dcb0b489593b53a09560f9
Rohan <rhnshiva@gmail.com>
2021-08-02 08:12:56 -0500

    fix `.detaignore` bug in windows

--- runtime/manager.go
+++ runtime/manager.go
@@ -341,6 +341,8 @@ func (m *Manager) isHidden(path string) (bool, error) {
 
 // should skip if the file or dir should be skipped
 func (m *Manager) shouldSkip(path string, runtime string) (bool, error) {
+	path = filepath.ToSlash(path)
+
 	// do not skip .detaignore file
 	if regexp.MustCompile(ignoreFile).MatchString(path) {
 		return false, nil
