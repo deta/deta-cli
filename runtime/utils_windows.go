801f823d17d6152dc4f36ff816635fddedf655ee
Aavash Shrestha <aavash09@gmail.com>
2022-02-18 09:49:12 +0100

    runtime: join manager root dir for filepath for hidden file check in windows, ignore unexisting files and files with bad attributes

--- runtime/utils_windows.go
+++ runtime/utils_windows.go
@@ -3,25 +3,35 @@
 package runtime
 
 import (
+	"os"
 	"path/filepath"
 	"strings"
 
 	"golang.org/x/sys/windows"
 )
 
-func isHiddenWindows(path string) (bool, error) {
+func (m *Manager) isHiddenWindows(path string) (bool, error) {
 	_, filename := filepath.Split(path)
 	// consider paths starting with "." also hidden in windows
 	if strings.HasPrefix(filename, ".") && filename != "." {
 		return true, nil
 	}
+
+	path = filepath.Join(m.rootDir, path)
+	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist){
+		// files that don't exist are taken as hidden
+		return true, nil
+	}
+
 	filePtr, err := windows.UTF16PtrFromString(path)
 	if err != nil {
 		return false, err
 	}
 	attrs, err := windows.GetFileAttributes(filePtr)
 	if err != nil {
-		return false, err
+		// ignore error and return true because if file attributes are bad or can't get file attributes
+		// the file should be ignored
+		return true, nil
 	}
 	return attrs&windows.FILE_ATTRIBUTE_HIDDEN != 0, nil
 }
