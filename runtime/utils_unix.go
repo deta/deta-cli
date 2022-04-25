a91b6956ac8474e7d2e76d417b8e54710f735368
Aavash Shrestha <aavash09@gmail.com>
2022-04-25 15:50:53 +0200

    runtime: use single hidden file check for both unix and windows

--- runtime/utils_unix.go
+++
@@ -1,9 +0,0 @@
-// +build !windows
-
-package runtime
-
-// mock function so that the compiler does not complain
-// when compiling for linux platforms
-func (m *Manager) isHiddenWindows(path string) (bool, error) {
-	return false, nil
-}
